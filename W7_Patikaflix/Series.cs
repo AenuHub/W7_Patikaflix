888afb21dc868684bd59732daab9f5bb0d0f6c25
AenuHub <jesuskane92@gmail.com>
2024-11-11 23:24:05 +0300

    create Series.cs

---
+++ W7_Patikaflix/Series.cs
@@ -0,0 +1,28 @@
+﻿namespace W7_Patikaflix;
+
+public class Series
+{
+    public string Title { get; set; }
+    public int ProductionYear { get; set; }
+    public List<string> Genres { get; set; }
+    public int ReleaseYear { get; set; }
+    public string Director { get; set; }
+    public string Platform { get; set; }
+    
+    public Series(string title, int productionYear, List<string> genres, int releaseYear, string director, 
+        string platform)
+    {
+        Title = title;
+        ProductionYear = productionYear;
+        Genres = genres;
+        ReleaseYear = releaseYear;
+        Director = director;
+        Platform = platform;
+    }
+    
+    // overriding ToString method to print series title, genres, director only
+    public override string ToString()
+    {
+        return $"Title: {Title}\nGenres: {string.Join(", ", Genres)}\nDirector: {Director}";
+    }
+}
